4576f466224b1f629cb4cd27f9a11ee8038d645f
simran maran <simranmaran10@gmail.com>
2025-12-23 23:42:17 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+from cloudinary.models import CloudinaryField
+
+class Jewellery(models.Model):
+    name = models.CharField(max_length=100)
+    image = CloudinaryField('image')
