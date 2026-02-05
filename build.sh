1ab47cd98485f32c90d2e227cc12ede7eba3676a
Simran  Maran <153758596+simranmaran@users.noreply.github.com>
2026-02-06 00:14:34 +0530

    Simplify build scripts to avoid nested invocation confusion

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Local helper: run from inside ./project
+# Render runs this script from the repository root.
+cd "$(dirname "$0")/project"
+
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
