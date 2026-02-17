4ce3790c5a1902729301686636b4a2ee7671b797
simranmaran <simranmaran10@gmail.com>
2026-02-17 16:00:08 +0530

    fix build script

--- project/build.sh
+++ project/build.sh
@@ -4,6 +4,10 @@ set -o nounset
 set -o pipefail
 
 cd "$(dirname "$0")/project"
+pip install --upgrade pip
+pip install --upgrade setuptools wheel
+
 pip install -r requirements.txt
+
 python manage.py migrate
 python manage.py collectstatic --noinput
