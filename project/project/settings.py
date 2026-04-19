368c5e2e8302c3dc375d248d768ca12b57d6a3f1
simranmaran <simranmaran10@gmail.com>
2026-04-19 21:35:31 +0530

    done

--- project/project/settings.py
+++ project/project/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-your-secret-key-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = "True"
 
 ALLOWED_HOSTS = ['*']
 
